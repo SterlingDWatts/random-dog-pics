6ebac23e3f432915deba2945b51506a4e9d5578b
Sterling Watts <SterlingDWatts@gmail.com>
2019-09-28 12:47:52 -0700

    Added fetch to Dog API

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const numInput = $('#choose-a-number')
 
+function displayResults(responseJson) {
+    console.log(responseJson)
+}
+
 function handleSubmitClick() {
     $('#num-dogs').on('click', function(e) {
         this.preventDefault;
@@ -7,7 +11,7 @@ function handleSubmitClick() {
             .then(response => response.json())
             .then(responseJson =>
                 displayResults(responseJson))
-            .catch(error => alert('Somethin went wrong. Try again later.'));
+            .catch(error => alert(error));
     })
 }
 
