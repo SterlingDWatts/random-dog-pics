7a5011ae29a40405a8aaed7338da2ec2e38cb278
Sterling Watts <SterlingDWatts@gmail.com>
2019-09-30 14:42:48 -0700

    Changed the API endpoint to get breed pictures

--- choose-a-breed/script.js
+++ choose-a-breed/script.js
@@ -1,1 +1,31 @@
 const breedInput = $('#choose-a-breed');
+
+function displayResults(responseJson) {
+    $('.dog-pictures').html(' ');
+    $('.dog-pictures').removeClass('hidden');
+    $('.full-or-not').removeClass('fullscreen')
+    for (let i = 0; i < responseJson.message.length; i ++) {
+        $('.dog-pictures').append('\n\t\t<img src="' + responseJson.message[i] + '">');
+    }
+}
+
+function fetchResponse() {
+    fetch(`https://dog.ceo/api/breed/${breedInput.val()}/images/random`)
+        .then(response => response.json())
+        .then(responseJson =>
+            displayResults(responseJson))
+        .catch(error => alert(error));
+}
+
+function handleSubmitClick() {
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        fetchResponse();
+    })
+}
+
+function loadStartFunctions() {
+    handleSubmitClick();
+}
+
+$(loadStartFunctions());
